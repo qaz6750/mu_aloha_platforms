66280ba77266a777e9c5be3e46fe7c923e5118de
KancyJoe <kancy2333@outlook.com>
2025-12-11 19:49:41 +0000

    displaycaller: update license header.

--- Platforms/AndromedaPkg/Driver/DisplayCallerDxe/DisplayCaller.c
+++ Platforms/AndromedaPkg/Driver/DisplayCallerDxe/DisplayCaller.c
@@ -7,7 +7,7 @@
 
  - License:
  * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2024. Woa-msmnile Authors. All rights reserved.
+ * Copyright (c) 2024-2025. Project-Aloha Authors. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
