297d94d89bc99c4fb9e0851e2ab5a9aa4f73dcd4
KancyJoe <kancy2333@outlook.com>
2026-01-08 15:27:41 +0000

    animalpkg: add basic clock enable support.

---
+++ Platforms/CranePkg/Library/ClockLib/clock_internal.h
@@ -0,0 +1,17 @@
+/** @file
+ *   Copyright (c) 2025-2026. Project Aloha Authors. All rights reserved.
+ *   Copyright (c) 2025-2026. Kancy Joe. All rights reserved.
+ *   SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+#include <Library/CrTargetClockLib.h>
+
+#include <Library/clock.h>
+#include <oskal/cr_assert.h>
+#include <oskal/cr_debug.h>
+#include <oskal/cr_interrupt.h>
+#include <oskal/cr_status.h>
+#include <oskal/cr_string.h>
+#include <oskal/cr_types.h>
+
