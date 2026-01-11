72dc9fb004f9ac5983e8ecf7fb0f41e40588c29b
Gustave Monce <gustave.monce@outlook.com>
2026-01-11 09:34:16 +0100

    Kona: MmuDetach

--- Silicon/QC/Sm8250/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
+++ Silicon/QC/Sm8250/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
@@ -1,24 +1,18 @@
-#include <PiPei.h>
-#include <Library/IoLib.h>
-#include <Library/PlatformPrePiLib.h>
-#include "PlatformUtils.h"
+/** @file
 
-VOID ConfigureIOMMUContextBankCacheSetting(
-    UINT32 ContextBankId, BOOLEAN CacheCoherent)
-{
-  UINT32 ContextBankAddr =
-      SMMU_BASE + SMMU_CTX_BANK_0_OFFSET + ContextBankId * SMMU_CTX_BANK_SIZE;
-  MmioWrite32(
-      ContextBankAddr + SMMU_CTX_BANK_SCTLR_OFFSET,
-      CacheCoherent ? SMMU_CCA_SCTLR : SMMU_NON_CCA_SCTLR);
-  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR0_0_OFFSET, 0);
-  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR0_1_OFFSET, 0);
-  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR1_0_OFFSET, 0);
-  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR1_1_OFFSET, 0);
-  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_MAIR0_OFFSET, 0);
-  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_MAIR1_OFFSET, 0);
-  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBCR_OFFSET, 0);
-}
+  Copyright (c) 2022-2024 DuoWoA authors
+
+  SPDX-License-Identifier: MIT
+
+**/
+#include <PiPei.h>
+
+#include <Library/IoLib.h>
+#include <Library/MemoryMapHelperLib.h>
+#include <Library/PlatformPrePiLib.h>
+
+#include "MmuDetach.h"
+#include "PlatformUtils.h"
 
 VOID SetWatchdogState(BOOLEAN Enable)
 {
@@ -27,13 +21,8 @@ VOID SetWatchdogState(BOOLEAN Enable)
 
 VOID PlatformInitialize(VOID)
 {
-  // Windows requires Cache Coherency for the UFS to work at its best
-  // The UFS device is currently attached to the main IOMMU on Context Bank 1
-  // (Previous firmware) But said configuration is non cache coherent compliant,
-  // fix it.
-  // ConfigureIOMMUContextBankCacheSetting(UFS_CTX_BANK, TRUE);
-  // TODO: Fix this!
-
   // Disable WatchDog Timer
   SetWatchdogState(FALSE);
+
+  MmuDetach();
 }
