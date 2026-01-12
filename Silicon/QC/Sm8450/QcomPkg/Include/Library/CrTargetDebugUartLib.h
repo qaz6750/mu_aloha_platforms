aadca1a6eda895a1bef83fb6d4b091b57d97cc5b
KancyJoe <kancy2333@outlook.com>
2026-01-12 07:20:32 +0000

    sm8450: add uart target lib.

---
+++ Silicon/QC/Sm8450/QcomPkg/Include/Library/CrTargetDebugUartLib.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Library/debug_uart.h>
+#include <oskal/common.h>
+
+CrDebugUartContext *CrTargetGetDebugUartContext(VOID);
---
+++ Silicon/QC/Sm8450/QcomPkg/Library/CrTargetLib/CrDebugUartTarget.c
@@ -0,0 +1,19 @@
+#include <Library/CrTargetDebugUartLib.h>
+#include <oskal/common.h>
+
+STATIC CrDebugUartContext mCrDebugUartContext = {
+    .BaseAddress = 0x99c000,
+    .InterruptNo = GIC_SPI(608),
+    .IsDebugUart = TRUE,
+    .Type        = DEBUG_UART_TYPE_GENI,
+    .BaudRate    = 115200,
+    .DataBits    = 0,
+    .Parity      = 0,
+    .StopBits    = 0,
+
+};
+
+CrDebugUartContext *CrTargetGetDebugUartContext(VOID)
+{
+  return &mCrDebugUartContext;
+}
