ae80ca46d138109679e5106a48c0e335120decd2
KancyJoe <kancy2333@outlook.com>
2026-02-10 11:03:22 +0000

    Platforms: Remove CranePkg

--- Platforms/CranePkg/Include/oskal/cr_memmap.h
+++
@@ -1,9 +0,0 @@
-#include <oskal/cr_debug.h>
-#include <oskal/cr_memory.h>
-#include <oskal/cr_status.h>
-
-CR_STATUS MapDeviceIORegion(IN UINTN BaseAddress, IN UINTN Size);
-CR_STATUS UnMapMemRegion(IN UINTN BaseAddress, IN UINTN Size);
-CR_STATUS MapMemRegion(
-    IN UINTN BaseAddress, IN UINTN Size, IN UINT64 Attributes,
-    IN UINT64 CacheAttr, IN UINT64 MemoryType);
